eb7e7ed2e036a589e9188bc876a81c9dda6a226d
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2023-11-11 23:42:59 -0300

    add doc to db module

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,6 @@
 /// handles all authentication and authorization functions of this application,
 /// including user registration and login
 pub mod auth;
+
+/// convenience mathods for connecting to and setting up the database
 pub mod db;
