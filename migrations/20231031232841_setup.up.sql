82c96f2f422db15c8a7efd5711b4e326235e933f
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2023-11-11 21:59:49 -0300

    update db setup migrations

--- migrations/20231031232841_setup.up.sql
+++ migrations/20231031232841_setup.up.sql
@@ -1,9 +1,13 @@
 CREATE TABLE if not exists users (
-    id bigserial NOT NULL UNIQUE PRIMARY KEY,
-    user_id uuid NOT NULL,
+    user_id uuid NOT NULL PRIMARY KEY,
     email text NOT NULL UNIQUE,
     username text NOT NULL UNIQUE,
     password bytea NOT NULL,
     salt bytea NOT NULL,
     creation_date timestamp with time zone NOT NULL
+);
+
+CREATE TABLE if not exists revoked_tokens (
+    token text NOT NULL UNIQUE,
+    expiration_date timestamp with time zone NOT NULL
 );
--- migrations/setup.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE if not exists users (
-    id bigserial NOT NULL UNIQUE PRIMARY KEY,
-    user_id uuid NOT NULL,
-    email text NOT NULL UNIQUE,
-    username text NOT NULL UNIQUE,
-    password bytea NOT NULL,
-    salt bytea NOT NULL
-);
